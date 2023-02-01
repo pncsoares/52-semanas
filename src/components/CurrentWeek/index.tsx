ed58e00a4a6680195e2910d5ddacbea902549063
Pedro Soares <pncsoares@gmail.com>
2023-02-01 15:36:46 +0000

    Simplify tailwind classes

--- src/components/CurrentWeek/index.tsx
+++ src/components/CurrentWeek/index.tsx
@@ -34,67 +34,67 @@ export default function CurrentWeek() {
 
   return (
     <>
-      <div className="flex flex-col flex-1 justify-center items-center content-center">
-        <div className="hero-content text-center flex">
-          <div className="max-w-md flex flex-col gap-3 sm:gap-4 md:gap-5 items-center">
-            <span className="text-4xl sm:text-5xl font-bold">Semana {currentWeekNumber}</span>
+      <div className="flex flex-col flex-1 justify-evenly content-evenly items-center text-center">
+        <span className="text-4xl sm:text-5xl font-bold">Semana {currentWeekNumber}</span>
 
-            <span>Montante inicial</span>
-            <div className="btn-group">
-              <button
-                className={`btn ${initialAmount === 0.25 ? 'btn-active' : ''} `}
-                onClick={() => changeInitialAmount(0.25)}
-              >
-                0,25 €
-              </button>
-              <button
-                className={`btn ${initialAmount === 0.5 ? 'btn-active' : ''} `}
-                onClick={() => changeInitialAmount(0.5)}
-              >
-                0,50 €
-              </button>
-              <button
-                className={`btn ${initialAmount === 0.75 ? 'btn-active' : ''} `}
-                onClick={() => changeInitialAmount(0.75)}
-              >
-                0,75 €
-              </button>
-              <button
-                className={`btn ${initialAmount === 1 ? 'btn-active' : ''} `}
-                onClick={() => changeInitialAmount(1)}
-              >
-                1,00 €
-              </button>
-            </div>
+        <div className="flex flex-col gap-1">
+          <span>Montante inicial</span>
+          <div className="btn-group">
+            <button
+              className={`btn ${initialAmount === 0.25 ? 'btn-active' : ''} `}
+              onClick={() => changeInitialAmount(0.25)}
+            >
+              0,25 €
+            </button>
+            <button
+              className={`btn ${initialAmount === 0.5 ? 'btn-active' : ''} `}
+              onClick={() => changeInitialAmount(0.5)}
+            >
+              0,50 €
+            </button>
+            <button
+              className={`btn ${initialAmount === 0.75 ? 'btn-active' : ''} `}
+              onClick={() => changeInitialAmount(0.75)}
+            >
+              0,75 €
+            </button>
+            <button
+              className={`btn ${initialAmount === 1 ? 'btn-active' : ''} `}
+              onClick={() => changeInitialAmount(1)}
+            >
+              1,00 €
+            </button>
+          </div>
+        </div>
 
-            <label>Montantes calculados 👇</label>
-            <div className="flex flex-col sm:flex-row gap-1 sm:gap-4">
-              <div className="stats bg-success text-success-content">
-                <div className="stat">
-                  <div className="stat-title">Até agora</div>
-                  <div className="stat-value">{currentAmount} €</div>
-                </div>
-              </div>
-
-              <div className="stats bg-primary text-primary-content">
-                <div className="stat">
-                  <div className="stat-title">No final do ano</div>
-                  <div className="stat-value">{totalAmount} €</div>
-                </div>
+        <div className="flex flex-col gap-1">
+          <label>Montantes calculados 👇</label>
+          <div className="flex flex-col sm:flex-row gap-1 sm:gap-4">
+            <div className="stats bg-success text-success-content">
+              <div className="stat">
+                <div className="stat-title">Até agora</div>
+                <div className="stat-value">{currentAmount} €</div>
               </div>
             </div>
 
-            <div className="flex flex-row justify-items-center gap-4">
-              <WeeksTableModalButton />
-              <WeeksTableModal
-                initialAmount={initialAmount}
-                currentWeekNumber={currentWeekNumber}
-              />
-
-              <HowToModalButton />
-              <HowToModal />
+            <div className="stats bg-primary text-primary-content">
+              <div className="stat">
+                <div className="stat-title">No final do ano</div>
+                <div className="stat-value">{totalAmount} €</div>
+              </div>
             </div>
           </div>
+        </div>
+
+        <div className="flex flex-row justify-items-center gap-4">
+          <WeeksTableModalButton />
+          <WeeksTableModal
+            initialAmount={initialAmount}
+            currentWeekNumber={currentWeekNumber}
+          />
+
+          <HowToModalButton />
+          <HowToModal />
         </div>
       </div>
     </>
